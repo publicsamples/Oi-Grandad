9ee751ade032f0291d38effe820752afc70af1bc
Rick <53375860+modularsamples@users.noreply.github.com>
2022-08-24 23:40:36 +0100

    CleanUp

--- oi grandad/Scripts/ScriptProcessors/gtest/Interface.js
+++ oi grandad/Scripts/ScriptProcessors/gtest/Interface.js
@@ -1,4 +1,22 @@
-Content.makeFrontInterface(600, 600);function onNoteOn()
+Content.makeFrontInterface(600, 600);
+
+const audio = Engine.createAndRegisterAudioFile(0);
+const var synth = Synth.getAudioSampleProcessor("Scriptnode Syntesiser1");
+
+const maps = Sampler.getSampleMapList();
+
+const slot = synth.getAudioFile(0);
+
+const var ComboBox1 = Content.getComponent("ComboBox1");
+ComboBox1.set("items", "");
+ComboBox1.set("items", maps.join("\n"));
+
+inline function onComboBox1Control(component, value)
+{
+	slot.loadFile("{XYZ::SampleMap}" + component.getItemText());
+};
+
+ComboBox1.setControlCallback(onComboBox1Control);function onNoteOn()
 {
 	
 }
