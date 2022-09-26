6bbe0319cb807366eb71af6a35b297ea28b087d5
Rick <53375860+modularsamples@users.noreply.github.com>
2022-09-26 01:25:15 +0100

    ui update

---
+++ oi grandad/DspNetworks/CodeLibrary/snex_osc/d.h
@@ -0,0 +1,33 @@
+template <int NumVoices> struct d
+{
+	// This macro enables C++ compilation to a snex_osc
+	SNEX_NODE(d);
+	
+	// This function will be called once per sample
+	float tick(double uptime)
+	{
+		return Math.fmod(uptime, 1.0);
+	}
+	
+	// This function will calculate a chunk of samples
+	void process(OscProcessData& d)
+	{
+		for (auto& s : d.data)
+		{
+			s = tick(d.uptime);
+			d.uptime += d.delta;
+		}
+	}
+	
+	// This can be used to initialise the processing if required.
+	void prepare(PrepareSpecs ps)
+	{
+	}
+	
+	void setExternalData(const ExternalData& d, int index)
+	{
+	}
+	template <int P> void setParameter(double v)
+	{
+	}
+};
