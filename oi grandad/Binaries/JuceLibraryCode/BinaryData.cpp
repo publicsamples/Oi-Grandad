1a27a65465081982c1aca6a068bc92b9c1643288
Rick <53375860+RickTaylor79@users.noreply.github.com>
2026-03-05 15:43:58 +0000

    newExport

--- oi grandad/Binaries/JuceLibraryCode/BinaryData.cpp
+++ oi grandad/Binaries/JuceLibraryCode/BinaryData.cpp
@@ -224,7 +224,6 @@ static const unsigned char temp_binary_data_3[] =
 const char* infoWarning_png = (const char*) temp_binary_data_3;
 
 
-const char* getNamedResource (const char* resourceNameUTF8, int& numBytes);
 const char* getNamedResource (const char* resourceNameUTF8, int& numBytes)
 {
     unsigned int hash = 0;
@@ -262,7 +261,6 @@ const char* originalFilenames[] =
     "infoWarning.png"
 };
 
-const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
 const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8)
 {
     for (unsigned int i = 0; i < (sizeof (namedResourceList) / sizeof (namedResourceList[0])); ++i)
--- oi grandad/Binaries/Source/PresetData.h
+++ oi grandad/Binaries/Source/PresetData.h
@@ -18,7 +18,7 @@ namespace PresetData
     const int           midiFilesSize = 28;
 
     extern const char*  preset;
-    const int           presetSize = 30427;
+    const int           presetSize = 30428;
 
     extern const char*  samplemaps;
     const int           samplemapsSize = 28;
