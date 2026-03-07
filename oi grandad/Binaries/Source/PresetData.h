055f37c5a1f3a8477c9f1897114de14951c81257
Rick <53375860+RickTaylor79@users.noreply.github.com>
2026-03-07 10:57:33 +0000

    Clean

--- oi grandad/Binaries/Source/PresetData.h
+++ oi grandad/Binaries/Source/PresetData.h
@@ -18,7 +18,7 @@ namespace PresetData
     const int           midiFilesSize = 28;
 
     extern const char*  preset;
-    const int           presetSize = 30475;
+    const int           presetSize = 30436;
 
     extern const char*  samplemaps;
     const int           samplemapsSize = 28;
