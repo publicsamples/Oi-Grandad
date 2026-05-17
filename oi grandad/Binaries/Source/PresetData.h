2ccd16935c1d20774a28705047f31e159d6cc9fb
Rick <53375860+RickTaylor79@users.noreply.github.com>
2026-05-17 22:05:10 +0100

    ui zoom

--- oi grandad/Binaries/Source/PresetData.h
+++ oi grandad/Binaries/Source/PresetData.h
@@ -18,7 +18,7 @@ namespace PresetData
     const int           midiFilesSize = 28;
 
     extern const char*  preset;
-    const int           presetSize = 31306;
+    const int           presetSize = 31282;
 
     extern const char*  samplemaps;
     const int           samplemapsSize = 28;
