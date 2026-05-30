e23506f252ccb96d405ce3509f3002e92123af89
Rick <53375860+RickTaylor79@users.noreply.github.com>
2026-05-30 23:14:02 +0100

    PresetData

---
+++ oi grandad/Binaries/Source/PresetData.h
@@ -0,0 +1,31 @@
+/* (Auto-generated binary data file). */
+
+#ifndef BINARY_PRESETDATA_H
+#define BINARY_PRESETDATA_H
+
+namespace PresetData
+{
+    extern const char*  externalFiles;
+    const int           externalFilesSize = 102358;
+
+    extern const char*  images;
+    const int           imagesSize = 10173763;
+
+    extern const char*  impulses;
+    const int           impulsesSize = 2575131;
+
+    extern const char*  midiFiles;
+    const int           midiFilesSize = 28;
+
+    extern const char*  preset;
+    const int           presetSize = 34983;
+
+    extern const char*  samplemaps;
+    const int           samplemapsSize = 28;
+
+    extern const char*  userPresets;
+    const int           userPresetsSize = 27;
+
+}
+
+#endif
