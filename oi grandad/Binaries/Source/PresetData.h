b13eb20fb217e7660ad30911cb28072c3ebe9b00
Rick <53375860+RickTaylor79@users.noreply.github.com>
2026-05-31 13:23:49 +0100

    Fix voice 4 poly fx

--- oi grandad/Binaries/Source/PresetData.h
+++ oi grandad/Binaries/Source/PresetData.h
@@ -18,7 +18,7 @@ namespace PresetData
     const int           midiFilesSize = 28;
 
     extern const char*  preset;
-    const int           presetSize = 35006;
+    const int           presetSize = 35020;
 
     extern const char*  samplemaps;
     const int           samplemapsSize = 28;
