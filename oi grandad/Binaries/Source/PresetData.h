05d97fa640ce68a479e2a7ac9d8511937bde291d
Rick <53375860+RickTaylor79@users.noreply.github.com>
2026-06-29 14:11:08 +0100

    Fix init preset

--- oi grandad/Binaries/Source/PresetData.h
+++ oi grandad/Binaries/Source/PresetData.h
@@ -18,7 +18,7 @@ namespace PresetData
     const int           midiFilesSize = 28;
 
     extern const char*  preset;
-    const int           presetSize = 35916;
+    const int           presetSize = 35987;
 
     extern const char*  samplemaps;
     const int           samplemapsSize = 28;
